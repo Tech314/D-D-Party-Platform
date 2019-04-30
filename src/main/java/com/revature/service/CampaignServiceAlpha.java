d5d4476ae1a98802af851d5622e7fe7aa1049f2e
Anthony Hromada <anthonyhromada@gmail.com>
2019-04-30 15:02:06 -0400

    Added annotations

--- src/main/java/com/revature/service/CampaignServiceAlpha.java
+++ src/main/java/com/revature/service/CampaignServiceAlpha.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.revature.model.Campaign;
 import com.revature.repository.CampaignRepository;
 
+@Service("campaignService")
 public class CampaignServiceAlpha implements CampaignService{
 	private static Logger logger = Logger.getLogger(CampaignServiceAlpha.class);
 	
