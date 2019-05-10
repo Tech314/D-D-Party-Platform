65db8810980c5bb6dd1d2bd4c1ee88f038cea1ee
Jeffafa1992 <nicholasrocksvold@yahoo.com>
2019-05-09 20:37:36 -0400

    resolved merge conflicts

--- src/main/java/com/revature/util/ClientMessageUtil.java
+++ src/main/java/com/revature/util/ClientMessageUtil.java
@@ -3,10 +3,9 @@ package com.revature.util;
 public class ClientMessageUtil {
 
 	public static final ClientMessage SOMETHING_WENT_WRONG = new ClientMessage("Something went wrong");
-<<<<<<< HEAD
+
 	public static final ClientMessage REGISTRATION_SUCCESSFUL = new ClientMessage("Registration Successful");
-=======
+
 	public static final ClientMessage CAMPAIGN_CREATED_SUCCESSFULLY = new ClientMessage("Campaign was successfully created");
 
->>>>>>> 036f0305905f85fa2ee7ed48eecc09897eb1239f
 }
